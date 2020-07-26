6e5b4b74d5e683570ead3e40cadeff0b6daa65ee
kenny <kenlnly@icloud.com>
2020-07-25 17:34:43 -0700

    Add guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,46 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng()
+        .gen_range(1, 101);
+
+    loop {
+        println!("Please input your guess.");
+
+        /*
+        * Note:
+        *
+        * In Rust, variables are immutable by default!
+        *
+        * Also, :: is an indication of associated functions,
+        * a.k.a. static methods.
+        */
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess
+            .trim()
+            .parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
